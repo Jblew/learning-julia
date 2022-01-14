e3d936757edb78c167f0e3c8df8c1dabeba9a521
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-01-14 13:23:58 +0100

    ci: julia run allow eval

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker run -it \
     -w /project \
     -v `pwd`/project:/project \
-    learning-julia /bin/bash -c "julia -e \"using Pkg; pkg\\\"activate .\\\"\" && julia \"${1}\""
+    learning-julia /bin/bash -c "julia -e \"using Pkg; pkg\\\"activate .\\\"\" && julia \"${1}\" \"${2}\""
