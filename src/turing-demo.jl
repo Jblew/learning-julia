9401fb937a0e84337afb0173867a3767432f1f05
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-01-14 14:02:36 +0100

    refactor: move files to src

--- src/turing-demo.jl
+++ src/turing-demo.jl
@@ -1,6 +1,8 @@
 using Turing
 using StatsPlots
 
+print("Begin turing-demo")
+
 # Define a simple Normal model with unknown mean and variance.
 @model function gdemo(x, y)
   s² ~ InverseGamma(2, 3)
@@ -9,11 +11,19 @@ using StatsPlots
   y ~ Normal(m, sqrt(s²))
 end
 
+print("Model done, running sampler")
+
 #  Run sampler, collect results
 chn = sample(gdemo(1.5, 2), HMC(0.1, 5), 1000)
 
+print("Sampler done, running describe")
+
+
 # Summarise results
 describe(chn)
+
+print("Describe done, saving fig")
+
 
 # Plot and save results
 p = plot(chn)
