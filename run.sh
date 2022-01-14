b7d77a7b565e6a34bc51c1e47a644ba21f482188
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-01-14 13:26:08 +0100

    fix: use functional pkg api instead of repl

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker run -it \
     -w /project \
     -v `pwd`/project:/project \
-    learning-julia /bin/bash -c "julia -e \"using Pkg; pkg\\\"activate .\\\"\" && julia \"${1}\" \"${2}\""
+    learning-julia /bin/bash -c "julia -e \"using Pkg; Pkg.activate(\\\".\\\")\" && julia \"${1}\" \"${2}\""
