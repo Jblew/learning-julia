05926caf1d249a740f0fe41a5a206db789b271c4
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-01-17 17:29:03 +0100

    feat: julia service example

---
+++ complex/deployable-julia-service/main.jl
@@ -0,0 +1,20 @@
+
+using Genie
+route("/") do
+    "Welcome to Genie, see <a href=\"/html\">HTML</a> or <a href=\"/json\">JSON</a>"
+end
+
+# Powerful high-performance HTML view templates
+using Genie.Renderer.Html
+route("/html") do
+    h1("Welcome to Genie!") |> html
+end
+
+# JSON rendering built in
+using Genie.Renderer.Json
+route("/json") do
+    (:greeting => "Welcome to Genie!") |> json
+end
+
+# Start the app!
+up(8888, async = false)
